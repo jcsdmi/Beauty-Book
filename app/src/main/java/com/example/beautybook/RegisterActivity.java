8eeb0a6a2d495fbc9acd66a278fec392bb377393
Jamee Cody <jamee3059@yahoo.com>
2020-04-13 21:51:01 -0400

    Working Splash Screen

---
+++ app/src/main/java/com/example/beautybook/RegisterActivity.java
@@ -0,0 +1,4 @@
+package com.example.beautybook;
+
+public class RegisterActivity {
+}
--- app/src/main/java/com/example/beautybook/SplashActivity.java
+++ app/src/main/java/com/example/beautybook/SplashActivity.java
@@ -6,14 +6,15 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        startActivity(new Intent(this,MainActivity.class));
-       /* Don't return to Splash Screen when back clicked */
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        /* Don't return to Splash Screen when back clicked */
         finish();
 
     }
